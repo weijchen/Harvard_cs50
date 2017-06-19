8a350b3aa8a25b2b00d83ab8f393cb6a999b6e76
weijchen <weijchen0919@gmail.com>
2017-06-19 17:39:34 +0800

    add java prac

---
+++ Java/helloworld.java
@@ -0,0 +1,17 @@
+class hello_world{
+	public static void main(String[] args){
+		System.out.println("Hello, world!");
+	}
+}
+ 
+class hello_java{
+	public static void main(String[] args){
+		System.out.println("Hello, Java!");
+	}
+}
+ 
+class hello_pcnoproblem{
+	public static void main(String[] args){
+		System.out.println("Hello, hi!");
+	}
+}
